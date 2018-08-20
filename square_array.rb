69d343f6a11bb9426886531426cca68545a73998
Jo Hooton <joanneelizabethhooton@gmail.com>
2018-08-20 13:34:29 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -3,5 +3,3 @@ def square_array(array)
   array.each { | x | numbers.push (x ** 2)}
   return numbers
 end
-/home/jo-hooton/square_array-london-web-082718/square_array.rb:5: syntax error, unexpected keyword_end, expecting '}'
-        from /usr/local/rvm/gems/ruby-2.3.1/bin/rspec:23:in `<main>'
